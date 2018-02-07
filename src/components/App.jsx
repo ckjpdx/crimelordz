ad0c961cb8fea3a661418cb2e84e3a44029be7cb
alspdx@gmail.com and Chris Johnson <chris@>
2018-02-07 12:06:01 -0800

    fixed nugs with initial setup, this setup is functional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return (
     <div>
+      <h1>App works!</h1>
       <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
+        <Route path='/'></Route>
       </Switch>
     </div>
   );
