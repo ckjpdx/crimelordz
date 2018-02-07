7f66130cb9e9ddc0cf411bfc8bd53786f447f661
alspdx@gmail.com and Chris Johnson <alspdx@gmail.com@>
2018-02-07 12:00:56 -0800

    initialize project and dev environment

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch, Route } from 'react-router-dom';
+
+function App(){
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={TicketList} />
+        <Route path='/newticket' component={NewTicketForm} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
